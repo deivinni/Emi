371b0f70edefd5242ceb096cc428320efea42e81
deivinni <deivinnimds3656@gmail.com>
2024-03-17 00:37:57 -0300

    Update config.js

--- src/settings/config.js
+++ src/settings/config.js
@@ -1,6 +1,6 @@
 const PREFIX = ".";
 const TIMEZONE = "America/Sao_Paulo";
 const STICKER_NAME = "Emi - BOT"
-const STICKER_AUTHOR = "+55 (35) 93619-1926";
+const STICKER_AUTHOR = process.env.BOT_PHONE;
 
 module.exports = { PREFIX, ALLOW_GROUPS, TIMEZONE, STICKER_NAME, STICKER_AUTHOR };
